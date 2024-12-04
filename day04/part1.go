6bdc936ec6332492fbafc052d178b5d37a319004
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2024-12-04 23:16:24 +0100

    added solution for part 1

--- day04/part1.go
+++ day04/part1.go
@@ -20,49 +20,45 @@ func Part1() {
 	}
 	defer file.Close()
 	reader := bufio.NewReader(file)
-	var result int
-	dic := make(map[int][]Point)
 
-	lineCounter := 0
+	grid := map[Point]string{}
+
+	linecount := 0
 	for {
 		line, _, err := reader.ReadLine()
 		if len(line) > 0 {
-			var points []Point
-			lineCounter = lineCounter + 1
+			linecount++
 			lineStr := string(line)
 			for i := 0; i < len(lineStr); i++ {
-				points = append(points, Point{
-					x: i,
-					y: lineCounter,
-				})
+				grid[Point{i, linecount}] = string(lineStr[i])
 			}
-			dic[lineCounter] = points
 		}
 		if err != nil {
 			break
 		}
 	}
 
-  
-
+	result := 0
+	for p := range grid {
+		result += strings.Count(strings.Join(findMatchingWord(p, grid), " "), findingText)
+	}
 	print(result)
 }
-
-func matchWord(line string) int {
-	wordCountInLine := strings.Count(line, findingText)
-	reverseXMAS := reverseString(findingText)
-	countTwo := strings.Count(line, reverseXMAS)
-	return wordCountInLine + countTwo
-}
-
-func matchLetters(line string) {
-
-}
-
-func reverseString(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+func findMatchingWord(point Point, grid map[Point]string) []string {
+	delta := []Point{
+		{0, -1}, {1, 0}, {0, 1}, {-1, 0},
+		{-1, -1}, {1, -1}, {1, 1}, {-1, 1},
 	}
-	return string(runes)
+
+	words := make([]string, len(delta))
+	for _, d := range delta {
+		word := ""
+		for n := 0; n < 4; n++ {
+			nextPoint := Point{x: point.x + d.x*n, y: point.y + d.y*n}
+			word += grid[nextPoint]
+		}
+		words = append(words, word)
+	}
+
+	return words
 }
