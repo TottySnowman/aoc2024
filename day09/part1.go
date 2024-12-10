9c94f174d360ca7899b307fb96be83ecb01de521
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2024-12-10 23:43:58 +0100

    rewritten the whole thing

--- day09/part1.go
+++ day09/part1.go
@@ -1,78 +1,57 @@
 package main
 
 import (
-	"bufio"
-	"os"
 	"strconv"
 	"strings"
 )
 
-var numbersOnly []int
-
+func toNumbers(input string) []int {
+	chars := strings.Split(input, "")
+	numbers := make([]int, len(chars))
+	for i, val := range chars {
+		numbers[i], _ = strconv.Atoi(val)
+	}
+	return numbers
+}
 func Part1() {
-	file, err := os.Open("puzzleInput.txt")
-	if err != nil {
-		panic("Failed to read the file")
+
+	input := "2333133121414131402"
+
+	numbers := toNumbers(input)
+
+	totalBlocksLength := 0
+	for i := 0; i < len(numbers); i += 2 { // Sum values at even indices
+		totalBlocksLength += numbers[i]
 	}
-	defer file.Close()
+	blocks := make([]int, totalBlocksLength)
+	currentBlockIndex := 0
+	reverseIndex := len(numbers) - 1
 
-	reader := bufio.NewReader(file)
-	var result int
-	var input string
-
-	var fileSystemStringy string
-	for {
-		line, _, err := reader.ReadLine()
-		if len(line) < 0 {
-			continue
-		}
-
-		if err != nil {
-			break
-		}
-		input = string(line)
-	}
-
-	isSpace := false
-	index := 0
-
-	for i := 0; i < len(input); i++ {
-		number, _ := strconv.Atoi(string(input[i]))
-
-		if isSpace {
-			for j := 0; j < number; j++ {
-				fileSystemStringy = fileSystemStringy + "."
+	for i := 0; i < len(numbers); i++ {
+		if i%2 == 0 {
+			for count := 0; count < numbers[i]; count++ {
+				blocks[currentBlockIndex] = i / 2
+				currentBlockIndex++
 			}
 		} else {
-			for j := 0; j < number; j++ {
-				numbersOnly = append(numbersOnly, index)
-				fileSystemStringy = fileSystemStringy + strconv.Itoa(index)
+			emptyFields := numbers[i]
+			for count := 0; count < emptyFields; count++ {
+				if currentBlockIndex >= totalBlocksLength {
+					break
+				}
+				for numbers[reverseIndex] == 0 {
+					reverseIndex -= 2
+				}
+				blocks[currentBlockIndex] = reverseIndex / 2
+				numbers[reverseIndex]--
+				currentBlockIndex++
 			}
-			index++
-		}
-		isSpace = !isSpace
-	}
-
-  for strings.Contains(fileSystemStringy, "."){
-    fileSystemStringy = replaceSingleDot(fileSystemStringy)
-  }
-  println(fileSystemStringy)
-	println(result)
-}
-
-func replaceSingleDot(input string) string {
-	number := numbersOnly[len(numbersOnly)-1]
-  numbersOnly = numbersOnly[:len(numbersOnly)-1]
-  println(numbersOnly)
-	inputBytes := []byte(input)
-  
-	for i := 0; i < len(inputBytes); i++ {
-		if inputBytes[i] == '.' {
-			inputBytes[i] = byte('0' + number)
-      break
 		}
 	}
+	checksum := 0
+	for i, val := range blocks {
+		checksum += i * val
+	}
 
-  inputBytes = inputBytes[:len(inputBytes)-1]
-	return string(inputBytes)
+	println(checksum)
 }
